5cdeb3e34ae8a220c176872fff2b6b65f8e06479
Diego2108 <castaneda.cortes.diego@gmail.com>
2024-01-06 01:34:42 -0600

    first commit

---
+++ src/main/java/com/example/pruebamifel/PruebamifelApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebamifel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebamifelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebamifelApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USUARIOS (id, nombre, email, telefono, edad) Values(1,'Diego','diego@mail.com', '7774578966', 28);
+INSERT INTO USUARIOS (id, nombre, email, telefono, edad) Values(2,'Juan', 'juan@gmail.com','7345789745',25);
+INSERT INTO USUARIOS (id, nombre, email, telefono, edad) Values(3,'Carlos', 'carlos@gmail.com','5578974523',26);
+INSERT INTO USUARIOS (id, nombre, email, telefono, edad) Values(4,'Andrea', 'andrea@gmail.com','7357861234',27);
+INSERT INTO USUARIOS (id, nombre, email, telefono, edad) Values(5,'Valeria', 'valeria@gmail.com','5678964512',25);
